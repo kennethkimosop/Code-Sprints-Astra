223be749bef761bc24277d6137a38554dcb7c557
Lexxy49 <kamau.albert23@students.dkut.ac.ke>
2025-05-10 13:12:33 +0300

    Update discount.py

--- problems/easy/discount/discount.py
+++ problems/easy/discount/discount.py
@@ -1,7 +1,12 @@
+# p = original price
+# d = percentage discount
 
-# p =original price
-#  d = percentage discount
+def getdiscountedPS(p, d):
+    discounted_price = p * (1 - d / 100)
+    return round(discounted_price, 2)
 
-def getdiscountedPS(p,d):
-    
-    return 0
+
+print(getdiscountedPS(1500, 50))  
+print(getdiscountedPS(89, 20))    
+print(getdiscountedPS(100, 75))   
+
