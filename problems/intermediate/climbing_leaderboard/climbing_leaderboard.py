5e53687c5dcd7a250bfdcae5964e047dba615bee
Gichura Edward <gichura.edward54@gmail.com>
2025-05-10 13:18:23 +0300

    Update climbing_leaderboard.py

--- problems/intermediate/climbing_leaderboard/climbing_leaderboard.py
+++ problems/intermediate/climbing_leaderboard/climbing_leaderboard.py
@@ -1,4 +1,20 @@
 
 def climbingLeaderboard(ranked, player):
-    # Write your code here
-    return []
+ 
+    unique_ranks = sorted(set(ranked), reverse=True)
+    result = []
+    n = len(unique_ranks)
+    i = n - 1  
+
+   
+    for score in player:
+     
+        while i >= 0 and score >= unique_ranks[i]:
+            i -= 1
+        result.append(i + 2)  
+    return result
+
+
+ranked = [100, 100, 50, 40, 40, 20, 10]
+player = [5, 25, 50, 120]
+print(climbingLeaderboard(ranked, player)) 
