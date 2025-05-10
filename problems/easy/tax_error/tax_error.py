5fca0c78393898b0498e95ac1fe2f415282dba2d
Gichura Edward <gichura.edward54@gmail.com>
2025-05-10 13:05:17 +0300

    Update tax_error.py

--- problems/easy/tax_error/tax_error.py
+++ problems/easy/tax_error/tax_error.py
@@ -1,9 +1,8 @@
 def calculate_tax(income):
     if income < 0:
-        return "Income can't be negative" 
+        return "Income can't be negative"
     tax_rate = 0.15
-    tax = 0
-    income * tax_rate  
+    tax = income * tax_rate  
     return tax
 
-print(calculate_tax(10000)) 
+print(calculate_tax(10000))  
